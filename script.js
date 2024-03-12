92c0908e8403a88e0c5517357b452780df2337c0
dark258 <carlosescobarzarzar@gmail.com>
2024-03-12 20:57:05 -0300

    1.0 version inicial del encriptador, código funcional para encriptar y desencriptar

--- script.js
+++ script.js
@@ -7,9 +7,12 @@
         u=ufat
 */
 
+// recibo el objeto del campo de texto para encriptar o desencriptar
 const campo_texto = document.querySelector("#texto-ingresado");
+// recibo el objeto del campo de texto para dejar el texto encriptado o desencriptado
 const campo_mensaje = document.querySelector("#campo-mensaje");
 
+// matriz con las letras y sus contrapartes encriptadas
 const matriz_cod = [
     ["e","enter"], 
     ["i","imes"],
@@ -17,14 +20,23 @@ const matriz_cod = [
     ["o","ober"], 
     ["u","ufat"]
 ]
+
+// función que dejará los mensajes de error o texto encriptado/desencriptado
 function mostrarMensaje(msg, color){
     campo_mensaje.innerHTML = msg;
     campo_mensaje.style.color = color;
 }
-//Función para validar el texto ingresado
-function validarTexto(){
+/*
+    Función para encriptar el texto
+    -  Revisa el texto ingresado
+    -  Si está vacío muestra un mensaje de error
+    -  Si no lo está, recorre el texto y reemplaza 
+       cada letra por su contraparte en la matriz "matriz_cod"
+*/
+function encriptarTexto(){
     const textoIngresado = campo_texto.value;
     
+    // en caso de recibir un texto vacío por parte del usuario
     if (textoIngresado === ""){
         mostrarMensaje("No ha ingresado nada", "red");
     }else {
@@ -42,8 +54,55 @@ function validarTexto(){
                 //Si no se encuentra la letra, agregarla tal cual
                 if (!encontrada) codificado+= textoIngresado[i];
         }
-        mostrarMensaje(`Codificación: ${codificado}`, "green")
+        mostrarMensaje(`Codificación: ${codificado}`, "green");
     }
 };
 
+function desencriptarTexto(){
+    const textoIngresado = campo_texto.value;
+    if (textoIngresado === ""){
+        mostrarMensaje("No ha ingresado nada","red");
+    } else {
+        let descifrada ="";
+        for (let i=0; i<textoIngresado.length; i++ ){
+            let noEncontrada = false;
+            let consonante = false;
+            for (let j = 0; j < matriz_cod.length && !noEncontrada; j++){
+                // encuentro la vocal inicial
+                if (matriz_cod[j][0].toLowerCase() == textoIngresado[i].toLowerCase()) {
+                    let k = 1
+                    // vocal = matriz_cod[j][0]
+                    for (; k < matriz_cod[j][1].length;k++){
+                        const kEsimaLetra =  matriz_cod[j][1][k];
+                        if (kEsimaLetra.toLowerCase() == textoIngresado[i+k]){
+                            continue;
+                        }else{
+                            for (let l=0; l < k; l++){
+                                descifrada += textoIngresado[i+l];
+                            }
+                            noEncontrada =true;
+                            break;
+                        }
+                    }
+                    // Si es false, quiere decir que hallé un trozo encriptado
+                    if (!noEncontrada){ 
+                        descifrada += matriz_cod[j][0];
+                    }
+                    i+=k-1;
+                    break;
+                }else{
+                    consonante = (j==matriz_cod.length-1);
+                    if (consonante){
+                        descifrada += textoIngresado[i];
+                    }
+                }
+            }
+
+        }
+        mostrarMensaje(`Decodificación: ${descifrada}`, "green");
+    }
+}
+
 //Eventos de teclado y clic del botón
+document.querySelector(".btnEncriptar").addEventListener("click",encriptarTexto);
+document.querySelector(".btnDesencriptar").addEventListener("click",desencriptarTexto);
